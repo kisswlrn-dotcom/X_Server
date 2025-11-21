39f25fb34adab51942ca99949fec8d1494855024
jasonlee <kisswlrn@gmail.com>
2025-11-21 15:21:11 +0900

    비밀번호 암호화 및 검증

--- data/auth.mjs
+++ data/auth.mjs
@@ -62,3 +62,8 @@ export async function login(userid, password) {
 
   return user;
 }
+
+export async function findByUserid(userid) {
+  const user = users.find((user) => user.userid === userid);
+  return user;
+}
--- router/auth.mjs
+++ router/auth.mjs
@@ -1,12 +1,36 @@
 import express from "express";
 import * as authController from "../controller/auth.mjs";
+import { body } from "express-validator";
+import { validate } from "../middleware/validator.mjs";
+("../middleware/validator.mjs");
 const router = express.Router();
 
+const validateLogin = [
+  body("userid")
+    .trim()
+    .isLength({ min: 4 })
+    .withMessage("아이디 최소 4자이상 입력")
+    .matches(/^[a-zA-Z0-9]+$/)
+    .withMessage("아이디 특수문자 사용불가"),
+  body("password")
+    .trim()
+    .isLength({ min: 4 })
+    .withMessage("비밀번호 최소 4자이상 입력"),
+  validate,
+];
+
+const validateSignup = [
+  ...validateLogin,
+  body("name").trim().notEmpty().withMessage("name을 입력"),
+  body("email").trim().isEmail().withMessage("이메일 형식 확인"),
+  validate,
+];
+
 // 회원 가입
-router.post("/signup", authController.signup);
+router.post("/signup", validateSignup, authController.signup);
 
 // 로그인
-router.post("/login", authController.login);
+router.post("/login", validateLogin, authController.login);
 
 // 로그인 유지
 
